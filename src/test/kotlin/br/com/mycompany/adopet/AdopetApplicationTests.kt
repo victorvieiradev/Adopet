2b92d83e1fb9d7c5b483892f8407f246bb9db880
Victor Vieira <contatovictorvieira@outlook.com>
2023-04-26 19:31:49 -0300

    projeto inicial

---
+++ src/test/kotlin/br/com/mycompany/adopet/AdopetApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.mycompany.adopet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdopetApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
